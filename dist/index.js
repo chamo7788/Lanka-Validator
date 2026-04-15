f97c6eeaaa7eefbc2c05d270f06670ac67d4945c
Chamodya Ganegamage <139947960+chamo7788@users.noreply.github.com>
2026-04-15 14:13:00 +0530

    Enhance exports in index files and update package.json for repository details

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.NIC = void 0;
+exports.Vehicle = exports.Mobile = exports.NIC = void 0;
 var NIC_1 = require("./NIC");
 Object.defineProperty(exports, "NIC", { enumerable: true, get: function () { return NIC_1.NIC; } });
+var Mobile_1 = require("./Mobile");
+Object.defineProperty(exports, "Mobile", { enumerable: true, get: function () { return Mobile_1.Mobile; } });
+var Vehicle_1 = require("./Vehicle");
+Object.defineProperty(exports, "Vehicle", { enumerable: true, get: function () { return Vehicle_1.Vehicle; } });
